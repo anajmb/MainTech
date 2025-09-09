eaef450112d4d38bc6add336d6a52b8f0e2053b8
unknown <pedromartes.apps@gmail.com>
2025-09-09 16:32:57 -0300

    corrigindo erros no controller

--- API/src/controller/employeesController.js
+++ API/src/controller/employeesController.js
@@ -1,5 +1,4 @@
 const { PrismaClient } = require("@prisma/client");
-const { login } = require("../../../../../LogiDev/LogiDevAPI/src/controllers/userController");
 const prisma = new PrismaClient();
 
 const employeesController = {
