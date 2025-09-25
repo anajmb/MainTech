61903bb967424fb386103d247c767a2f8f262e8c
marisilva7 <mariani.alves27@outlook.com>
2025-09-25 13:13:13 -0300

    estilizando pagina equipes

--- Mobile/app/(tabs)/home/equipes.tsx
+++ Mobile/app/(tabs)/home/equipes.tsx
@@ -1,7 +1,8 @@
 import SetaVoltar from "@/components/setaVoltar";
 import { TabsStyles } from "@/styles/globalTabs";
 import { Link } from "expo-router";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Wrench } from "lucide-react-native";
+import { ScrollView, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function Equipes() {
     return (
@@ -22,13 +23,46 @@ export default function Equipes() {
                 <View style={style.groupEqui}>
                     {/* icone grupo, cor random */}
 
+                    <View style={style.iconeEquipe}>
+                        <Wrench color="white" />
+                    </View>
+
                     <View style={style.infoEqui}>
                         <Text style={style.tituloEqui}>Equipe de Manutenção</Text>
                         <Text style={style.descricaoEqui}>Equipe responsável pela manutenção de máquinas e equipamentos</Text>
                     </View>
 
-                    <Text style={style.quantMembro}>8 membros</Text>
                     {/* <Link href={'#'} style={style.verEquipe}>Ver equipe</Link> */}
+                </View>
+                <View style={style.footerCard}>
+                    <Text style={style.quantMembro}>8 membros</Text>
+                    <Text style={style.verEquipe}>Ver equipe</Text>
+                </View>
+            </View>
+
+            <View style={style.cardAdicionar}>
+                <Text style={style.tituloAdicionar}>Adicionar Membro</Text>
+                <View style={{ marginTop: 12 }}>
+                    <Text style={style.labelAdicionar}>Nome da equipe:</Text>
+                    <TextInput
+                        style={style.inputAdicionar}
+                        placeholder="Selecionar"
+                        placeholderTextColor="#8B8686"
+                    />
+                </View>
+                <View style={{ marginTop: 12 }}>
+                    <Text style={style.labelAdicionar}>E-mail:</Text>
+                    <TextInput
+                        style={style.inputAdicionar}
+                        placeholder="ex: cida@email.com"
+                        placeholderTextColor="#8B8686"
+                        keyboardType="email-address"
+                    />
+                </View>
+                <View style={{ alignItems: "center", marginTop: 18 }}>
+                    <View style={style.botaoAdicionar}>
+                        <Text style={style.textoBotaoAdicionar}>Adicionar membro</Text>
+                    </View>
                 </View>
             </View>
 
@@ -38,24 +72,125 @@ export default function Equipes() {
 
 const style = StyleSheet.create({
     card: {
+        backgroundColor: "#fff",
+        borderRadius: 16,
+        padding: 16,
+        marginVertical: 8,
+        marginHorizontal: 8,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 3,
 
     },
     groupEqui: {
-
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 12,
     },
     infoEqui: {
+        flex: 1,
 
     },
     tituloEqui: {
+        fontSize: 16,
+        color: "#000000",
+        marginBottom: 2,
+        fontWeight: "500",
 
     },
     descricaoEqui: {
-
-    },
-    quantMembro: {
+        fontSize: 15,
+        color: "#8B8686",
+        marginBottom: 8,
+        fontWeight: "400",
+        marginTop: 10,
 
     },
     verEquipe: {
+        fontSize: 13,
+        marginTop: 4,
+        color: "#CE221E",
+        marginRight: 12,
+        fontWeight: "500",
 
-    }
+    },
+    quantMembro: {
+        backgroundColor: "#f1f1f1",
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        paddingVertical: 4,
+        fontSize: 12,
+        color: "#444",
+        alignSelf: "flex-start",
+        marginRight: 12,
+
+    },
+    footerCard: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginTop: 12,
+    },
+    iconeEquipe: {
+        width: 50,
+        height: 50,
+        borderRadius: 10,
+        backgroundColor: "#1E9FCE",
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 15,
+
+    },
+    cardAdicionar: {
+        backgroundColor: "#fff",
+        borderRadius: 16,
+        padding: 18,
+        marginVertical: 12,
+        marginHorizontal: 8,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.13,
+        shadowRadius: 4,
+        elevation: 3,
+    },
+    tituloAdicionar: {
+        fontSize: 18,
+        fontWeight: "500",
+        color: "#222",
+        marginBottom: 8,
+    },
+    labelAdicionar: {
+        fontSize: 15,
+        color: "#222",
+        marginBottom: 4,
+        fontWeight: "400",
+    },
+    inputAdicionar: {
+        backgroundColor: "#F5F5F5",
+        borderRadius: 8,
+        paddingHorizontal: 12,
+        paddingVertical: 10,
+        justifyContent: "center",
+    },
+    inputTextAdicionar: {
+        color: "#8B8686",
+        fontSize: 14,
+    },
+    botaoAdicionar: {
+        backgroundColor: "#CE221E",
+        borderRadius: 8,
+        paddingVertical: 10,
+        paddingHorizontal: 24,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 8,
+    },
+    textoBotaoAdicionar: {
+        color: "#fff",
+        fontSize: 15,
+        fontWeight: "500",
+    },
+
 })
