69a0baa243866d731f54fff20bd27e20cff079c8
Gabrieyfs <felixdesouzagabriely@gmail.com>
2025-09-25 09:09:01 -0300

    add páginas Criar Equipe e Usuario

---
+++ Mobile/app/(tabs)/home/criarEquipe.tsx
@@ -0,0 +1,13 @@
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
+export default function CriarEquipe() {
+    return (
+        <ScrollView>
+            <Text>Criar Equipe</Text>
+        </ScrollView>
+    )
+}
+
+const style = StyleSheet.create({
+    
+})
