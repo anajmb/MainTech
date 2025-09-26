2fd753993216e26fd347b7f964d19c2366c92e30
Ana Julia Mathias <estrelaanamb0108@gmail.com>
2025-09-26 18:35:14 -0300

    Adicionando leitor de qrCode

--- Mobile/app/(tabs)/QRCode/index.tsx
+++ Mobile/app/(tabs)/QRCode/index.tsx
@@ -1,21 +1,69 @@
-import { Text, View, Button, Modal} from "react-native";
+import { Text, View, Button, Modal, StyleSheet, Alert} from "react-native";
 import {CameraView, useCameraPermissions} from "expo-camera"
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function QRCode() {
 
     const [modalIsVisible, setModalIsVisible] = useState(false)
+    const [permission, requestPermission] = useCameraPermissions()
+
+    const qrCodeLock = useRef(false)
+
+     async function handleOpenCamera() {
+      try {
+        const { granted } = await requestPermission()
+        if(!granted) {
+          return Alert.alert("Camera", "Voce precisa habilitar o uso da camera")
+        }
+
+
+        setModalIsVisible(true)
+        qrCodeLock.current = false
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    function handdleQRCodeRead(data: string) {
+      setModalIsVisible(false)
+      Alert.alert("QRCode", `O código lido foi: ${data}`)
+    }
+    
+    
     return (
-        <View>
-           {/* <Button title="Ler QRCode" style={{top: 100}}  /> */}
+        <View style={styles.container}>
+            <Button title="Ler QRCode" onPress={handleOpenCamera}/> 
 
-           <Modal visible={modalIsVisible}>
-            <CameraView style={{flex: 1}} facing="back" />
+           <Modal visible={modalIsVisible} style={{flex: 1}} animationType="slide">
+            <CameraView style={{flex: 1}} facing="back"
+             onBarcodeScanned={({ data}) => {
+              if (data && !qrCodeLock.current) {
+                qrCodeLock.current = true
+                setTimeout(() => handdleQRCodeRead(data), 1000)
+             }}}/>
 
-            <View>
+            <View style={styles.footer}>
               <Button onPress={() => setModalIsVisible(false)} title="Cancelar" />  
             </View>
            </Modal>
         </View>
     )
-}
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#fff"
+    },
+    buttonQRCode: {
+      top: 100
+    },
+    footer: {
+      position: "absolute",
+      bottom: 32,
+      left: 32,
+      right: 32,
+    }
+  });
