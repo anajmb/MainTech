722b21eb618a38f08eef526439f59e38c603faff
marisilva7 <mariani.alves27@outlook.com>
2025-10-09 11:24:40 -0300

    fazendo pagina historico

--- Mobile/app/(tabs)/home/agenda.tsx
+++ Mobile/app/(tabs)/home/agenda.tsx
@@ -14,7 +14,7 @@ export default function Agenda() {
             <View style={TabsStyles.headerPrincipal}>
                 <SetaVoltar />
 
-                <       View style={TabsStyles.conjHeaderPrincipal}>
+                <View style={TabsStyles.conjHeaderPrincipal}>
                     <Text style={TabsStyles.tituloPrincipal}>Agenda</Text>
                     <Text style={TabsStyles.subtituloPrincipal}>Sexta-feira, 15 de agosto</Text>
                 </View>
@@ -22,14 +22,14 @@ export default function Agenda() {
 
             {/* input data */}
 
-            <Link href={"/(tabs)/home/calendario"}>
             <View style={style.cardCalen}>
+            <Link href={"/(tabs)/home/calendario"}>
                 <View style={style.groupCalen}>
                     <Calendar color={'#6797F0'} />
                     <Text style={style.tituloCardCalen}>Calendário</Text>
                 </View>
-            </View>
             </Link>
+            </View>
 
             <View style={style.groupEvent}>
                 <Text style={style.tituloEvent}>Evento de hoje</Text>
@@ -55,6 +55,11 @@ export default function Agenda() {
 const style = StyleSheet.create({
     cardCalen: {
         alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        marginTop: 18,
+        marginBottom: 18,
+
 
     },
     groupCalen: {
